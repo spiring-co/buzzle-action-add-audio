652118751d0ea5dbeb0246aec27de85c21a5f825
AbdulBsit <abasit325@gmail.com>
2021-03-13 15:05:13 +0530

    added fadein and fade out

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fetch = require("node-fetch");
 const nfp = require("node-fetch-progress");
 const fs = require("fs");
 const path = require("path");
+const ffprobe = require('ffprobe');
+const ffprobeStatic = require('ffprobe-static');
 
 const getBinary = (job, settings) => {
   return new Promise((resolve, reject) => {
@@ -67,7 +69,7 @@ const getBinary = (job, settings) => {
   });
 };
 
-module.exports = (job, settings, { input, audio, output, onStart, onComplete }) => {
+module.exports = (job, settings, { input, audio, output, fadeOutDuration = 3, fadeIntDuration = 2, onStart, onComplete }) => {
   onStart()
   return new Promise((resolve, reject) => {
     input = input || job.output;
@@ -83,15 +85,19 @@ module.exports = (job, settings, { input, audio, output, onStart, onComplete }) 
     if (!path.isAbsolute(output)) output = path.join(job.workpath, output);
 
     settings.logger.log(
-      `[${job.uid}] starting action-add-audio on [${input}] `
+      `[${job.uid}] starting buzzle-action-add-audio on [${input}] `
     );
 
-    getBinary(job, settings).then((p) => {
+    getBinary(job, settings).then(async (p) => {
       ffmpeg.setFfmpegPath(p);
+      //get video duration and fade audio from start and last 5 sec
+      const videoDetails = (await ffprobe(input, { path: ffprobeStatic.path })).streams.find(({ codec_type }) => codec_type === 'video')
+      const fadeOutStartAt = parseInt(videoDetails.duration) - fadeOutDuration
       ffmpeg()
         .input(input)
         .input(audio)
-        .outputOptions(["-map 1:a:0", "-map 0:v:0", "-shortest"])
+        .outputOptions(["-map 1:a:0", "-map 0:v:0", "-shortest",
+          `-af afade=t=in:st=0:d=${fadeIntDuration},afade=t=out:st=${fadeOutStartAt}:d=${fadeOutDuration}`])
         .on("error", function (err, stdout, stderr) {
           console.log("join audio video failed: " + err.message);
           onComplete()
--- sandbox.js
+++ sandbox.js
@@ -1,62 +1,20 @@
-var ffmpeg = require("fluent-ffmpeg");
-const https = require("https");
-const fs = require("fs");
-
+const mergeThumbnail = require("./index")
 /**
  * Download a resource from `url` to `dest`.
  * @param {string} url - Valid URL to attempt download of resource
  * @param {string} dest - Valid path to save the file.
  * @returns {Promise<void>} - Returns asynchronously when successfully completed download
  */
-function download(url, dest) {
-  return new Promise((resolve, reject) => {
-    const request = https.get(url, (response) => {
-      if (response.statusCode === 200) {
-        const file = fs.createWriteStream(dest, { flags: "wx" });
-        file.on("finish", () => resolve());
-        file.on("error", (err) => {
-          file.close();
-          if (err.code === "EEXIST") reject("File already exists");
-          else fs.unlink(dest, () => reject(err.message)); // Delete temp file
-        });
-        response.pipe(file);
-      } else if (response.statusCode === 302 || response.statusCode === 301) {
-        //Recursively follow redirects, only a 200 will resolve.
-        download(response.headers.location, dest).then(() => resolve());
-      } else {
-        reject(
-          `Server responded with ${response.statusCode}: ${response.statusMessage}`
-        );
-      }
-    });
-
-    request.on("error", (err) => {
-      reject(err.message);
-    });
-  });
-}
-ffmpeg.setFfmpegPath("/Users/harshbhatia/code/sandbox/ffmpeg-b4.2.2");
-ffmpeg.setFfprobePath("/Users/harshbhatia/Desktop/ffprobe");
-const output = "output.mp4";
-// download("https://firebasestorage.googleapis.com/v0/b/bulaava-functions-test.appspot.com/o/bulaavaAssets%2F1594970137219.mpeg?alt=media&token=c22ce4e7-69b8-435e-86a8-3454322daf9f", 'abc.mpeg')
-ffmpeg()
-  //input files
-  .input(
-    "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4"
-  )
-  .input(
-    "https://firebasestorage.googleapis.com/v0/b/bulaava-functions-test.appspot.com/o/bulaavaAssets%2F1594972730964.mp3?alt=media&token=ba12c8d5-1cd3-4443-a7e3-374f6f6745c9"
-  )
-  // .input("https://firebasestorage.googleapis.com/v0/b/bulaava-functions-test.appspot.com/o/bulaavaAssets%2F1594970137219.mpeg?alt=media&token=c22ce4e7-69b8-435e-86a8-3454322daf9f")
-  .outputOptions(["-map 1:a:0", "-map 0:v:0", "-shortest"])
-  .on("error", function (err, stdout, stderr) {
-    console.log("join audio video failed: " + err.message);
-  })
-  .on("progress", function (value) {
-    console.log("In Progress.." + value);
-  })
-  .on("end", function () {
-    //save
-    console.log("added audio successfully");
-  })
-  .save(output);
+const output = "musicWithFade.mp4";
+let audio = 'test.mp3'
+const input = 'video.mp4'
+let started = Date.now()
+mergeThumbnail(
+  { output: "C:\\Users\\Utkarsh\\Desktop", workpath: "C:\\Users\\Utkarsh\\Desktop" }, { logger: { log: console.log }, workpath: "C:\\Users\\Utkarsh\\Desktop" }, {
+  input, audio, output,
+  onStart: () => {
+    console.log("Started")
+    started = Date.now()
+  },
+  onComplete: () => console.log("completed in", (Date.now() - started) / 1000, " secs")
+})
